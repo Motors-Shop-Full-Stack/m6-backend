9f2c0460ad12d51796f222cbb36febe961578c2d
kennedybm <kennedy_muniz@hotmail.com>
2022-10-18 16:44:47 -0300

    feat: created error management middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import "express-async-errors";
 import "reflect-metadata";
 import cors from "cors";
 import routesTest from "./routers";
+import handleAppErrorMiddleware from "./middlewares/handleAppError.middlewares";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 //Routes
 app.use("/test", routesTest);
+app.use(handleAppErrorMiddleware);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("App runing");
