0ebaf5d45e34d3fdde3fc2b78132bbf2dc0b9883
kennedybm <kennedy_muniz@hotmail.com>
2023-03-01 13:59:02 -0300

    Feat: adding email and isActive to global user object

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -4,7 +4,9 @@ declare global {
   namespace Express {
     interface Request {
       user: {
-        userId: string;
+        id: string;
+        email: string;
+        isActive: boolean;
       };
     }
   }
